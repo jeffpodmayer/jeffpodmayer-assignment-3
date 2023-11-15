d338a94953c580d02e02df5f26e1cc51db63a470
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2023-11-15 13:50:51 -0800

    Still updating and trying.....no luck

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -1,47 +1,11 @@
 package com.coderscampus;
 
-import java.util.Scanner;
-
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
 		UserService userService = new UserService();
-
-		Scanner scanner = new Scanner(System.in);
-
-		User[] users = userService.readFile();
 		
-		int totalInputs = 4;
-		int inputCount = 0;
-
-		while (inputCount < totalInputs) {
-			System.out.println("Enter your email:");
-			String userEmail = scanner.nextLine();
-
-			System.out.println("Enter your password:");
-			String userPassword = scanner.nextLine();
-
-
-			for (User user : users) { // program not recognizing correct input of userEmail/userPassword
-				if (userEmail.equals(user.getUsername()) && userPassword.equals(user.getPassword())) { // is this correct way to check for match?
-					System.out.println("Welcome: " + user.getName());
-					break;
-
-				} else if(!userEmail.equals(user.getUsername()) || !userPassword.equals(user.getPassword())) { 
-					System.out.println("Invalid login, please try again!");
-					inputCount++;
-					
-					System.out.println("Enter your email:");
-					userEmail = scanner.nextLine();
-
-					System.out.println("Enter your password:");
-					userPassword = scanner.nextLine();
-					
-						}
-					
-					}
-						System.out.println("Too many failed login attempts, you are locked out!");
-				}
+		userService.validateInput();
 
 			}
 			
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -6,15 +6,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserService {
 
 	BufferedReader fileReader = null;
+	Scanner scanner = new Scanner(System.in);
+	List<User> users = new ArrayList<User>();
 
 	public User[] readFile() {
 
-		List<User> users = new ArrayList<>();
 		BufferedReader fileReader = null;
+		String userInfo;
 
 		try {
 			fileReader = new BufferedReader(new FileReader("data.txt"));
@@ -22,12 +25,11 @@ public class UserService {
 			System.out.println("FileNotFound Exception.");
 			e.printStackTrace();
 		}
-		String userInfo;
 
 		try {
 			while ((userInfo = fileReader.readLine()) != null) {
 				String[] userData = userInfo.split(",");
-				User user = createUser(userData); // error may be happening here?
+				User user = createUser(userData);
 				users.add(user);
 			}
 		} catch (IOException e) {
@@ -44,9 +46,45 @@ public class UserService {
 		return users.toArray(new User[0]);
 	}
 
-	public User createUser(String[] userData) { // error may be happening in this method?
+	public User createUser(String[] userData) {
 		if (userData != null && userData.length == 3) {
-			return new User(userData[0], userData[1], userData[2]); // references constructor in "User" class...I think
+			return new User(userData[0], userData[1], userData[2]);
+		}
+		return null;
+	}
+
+	public User validateInput() {
+
+		int totalInputs = 4;
+		int inputCount = 0;
+
+		while (inputCount <= totalInputs) {
+			System.out.println("Enter your email:");
+			String userEmail = scanner.nextLine();
+
+			System.out.println("Enter your password:");
+			String userPassword = scanner.nextLine();
+
+			boolean loginSuccessful = false;
+
+			for (User user : users) {
+				if (userEmail.equals(user.getUsername()) && userPassword.equals(user.getPassword())) {
+					System.out.println("Welcome: " + user.getName());
+					loginSuccessful = true;
+					break;
+
+				}
+			}
+
+			if (!loginSuccessful || inputCount == 3) {
+				System.out.println("Invalid login, please try again!");
+				inputCount++;
+
+			} 
+			
+			System.out.println("Too many failed login attempts, you are locked out!");
+			break;
+		
 		}
 		return null;
 	}
