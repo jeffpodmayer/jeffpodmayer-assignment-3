0baebe4d091ba7f3b633f188a0db2a1601a56015
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2023-11-14 16:38:28 -0800

    Updated Commit

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -6,26 +6,41 @@ public class UserLoginApplication {
 
 	public static void main(String[] args) {
 		UserService userService = new UserService();
+		
 		Scanner scanner = new Scanner(System.in);
 		
-		String email;
-		String password;
-		String name;
+		System.out.println("Enter your email:");
+		String userEmail = scanner.nextLine();
 
+		System.out.println("Enter your password:");
+		String userPassword = scanner.nextLine();
+		
 		for (int i = 0; i < 5; i++) {
-			if(email != userService.readFile() && password != userService.readFile()) {
+			if (userEmail == username && userPassword == userService.readFile(){
+				System.out.println("Welcome: " + name);
+				break;
+				
+			} else if (userEmail != userService.readFile() && userPassword != userService.readFile()) {
+				System.out.println("Invalid login, please try again!");
+				
 				System.out.println("Enter your email:");
-				String userEmail = scanner.nextLine();
+				userEmail = scanner.nextLine();
 
 				System.out.println("Enter your password:");
-				String userPassword = scanner.nextLine();
+				userPassword = scanner.nextLine();
+				
 			}
-			
+				
 		}
+		}
+		
+		
+
+		
 
 		// Use scanner to prompt for username/password
 		// Call method for determining match from UserService class
 		// FOR loop for logic for number of times user gets to attempt login
 	}
 
-}
+
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -6,59 +6,63 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserService {
 
 	BufferedReader fileReader = null;
 
-	public void readFile() throws FileNotFoundException {
+	public void readFile() {
+
+		List<User> users = new ArrayList<>();
+		BufferedReader fileReader = null;
 		
-		List <String[]> users = new ArrayList<String[]>();
-		
-		
-			BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"));
-			String userInfo;
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFound Exception.");
+			e.printStackTrace();
+		}
+		String userInfo;
+
+		try {
+			while ((userInfo = fileReader.readLine()) != null) {
+				String[] userData = userInfo.split(",");
+				User user = new User(userData[0], userData[1], userData[2]); // is this the same as the createUser method below?
+				users.add(user);
+				return;
 			
-			try {
-				while ((userInfo = fileReader.readLine()) != null) {
-					String[] user = { userInfo };
-					users.add(user);
-				}
-			} catch (IOException e) {
-				System.out.println("IO Exception!");
-				e.printStackTrace();
-			} finally {
+			}
+		} catch (IOException e) {
+			System.out.println("IO Exception!");
+			e.printStackTrace();
+		} finally {
 			try {
 				fileReader.close();
 			} catch (IOException e) {
 				System.out.println("IO Exception!");
 				e.printStackTrace();
 			}
-		  } 
-		
-		  for(String[] userArray : users) {
-			  String userLine = userArray[0];
-			  
-			  String[] userData = userLine.split(",");
-			  
-			  String username = userData[0];
-			  String password = userData[1];
-			  String name = userData[2];
-			  
-			  User user = new User();
-			  user.setUsername(username);
-			  user.setPassword(password);
-			  user.setName(name);
-		  }
 		}
-		
-	public User createUser(String[] userInfo) {
-		 
-	}
-		
 
+  }
+	
+	public User createUser(String[] userData) {
+		if (userData != null && userData.length >= 3) {
+            String username = userData[0];
+            String password = userData[1];
+            String name = userData[2];
+            
+           return new User(username, password, name);
+
+        } else {
+            return null;
+		
+	 }
+}
+	
+
+	public void inputCheck() {
+	}
 }
 
- 
-// read data from file and create User array
-// validate of inputed username and password matches what is in User Array
