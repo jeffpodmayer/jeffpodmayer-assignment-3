a008207bb9eefaafeb7393af3347866c98628044
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2023-12-11 09:09:42 -0800

    Commented out unused code

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -10,15 +10,15 @@ public class UserService {
 
 	// creates a user and assigns it to each index of the array object, returns new
 	// User
-	public User createUser(String[] userData) {
-		String username = userData[0];
-		String password = userData[1];
-		String name = userData[2];
-
-		User user = new User(username, password, name);
-		return user;
-
-	}
+////	public User createUser(String[] userData) {
+//		String username = userData[0];
+//		String password = userData[1];
+//		String name = userData[2];
+//
+//		User user = new User(username, password, name);
+//		return user;
+//
+//	}
 
 	// validates if userEmail & userPassword equal what the user entered
 	public User getUserByUsernameAndPassword(String userEmail, String userPassword) {
